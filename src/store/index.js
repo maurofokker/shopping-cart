c22f942a01d322327a4a9c5742a59576bb82eb09
Mauricio Galdames <mgaldamesc.azurian@bancochile.cl>
2019-02-25 16:08:14 -0300

    split vuex store separating actions in different file bc index.js is getting big

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,6 @@
 import Vuex from 'vuex'
 import Vue from 'vue'
-import shop from '@/api/shop'
-import { resolve } from 'uri-js';
+import actions from './actions'
 
 // tells Vue to use Vuex
 Vue.use(Vuex)
@@ -47,44 +46,7 @@ export default new Vuex.Store({
     }
   },
 
-  actions: { // same as components methods
-    fetchProducts (context) {
-      return new Promise((resolve, reject) => {
-        shop.getProducts(products => {
-          // we need to commit a mutation passing the name of the mutation
-          // and passing the payload that is the returned products
-          context.commit('setProducts', products)
-          resolve()
-        })
-      })
-    },
-    // it is better to use discrete names to know what the action or mutation does by reading its name
-    addProductToCart ({state, getters, commit}, product) {
-      if (getters.productIsInStock(product)) {
-        // find cartItem
-        const cartItem = state.cart.find(item => item.id === product.id)
-        if (!cartItem) { // push product to cart
-          commit('pushProductToCart', product.id)
-        } else { // increment item quantity
-          commit('incrementItemQuantity', cartItem)
-        }
-        commit('decrementProductInventory', product) // pass incoming product to decrement inventory
-      }
-    },
-
-    checkout ({commit, state}) {
-      shop.buyProducts(
-        state.cart, // 1st products / cart
-        () => { // 2nd success cb
-          commit('emptyCart')
-          commit('setCheckoutStatus', 'success')
-        },
-        () => { // 3rd failure cb
-          commit('setCheckoutStatus', 'fail')
-        }
-      )
-    }
-  },
+  actions,
 
   mutations: { // responsible for setting and updating the state
     setProducts (state, products) { // first parameter will always be the current state and the second the payload
